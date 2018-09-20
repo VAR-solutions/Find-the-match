cd26f8e8eb057e90a0911f3112c5c7dae7f84603
Antew <ananya.tewari.7@gmail.com>
2018-09-21 00:39:32 +0530

    javascript added

---
+++ matches.js
@@ -0,0 +1,15 @@
+var tiles = document.querySelectorAll(".tile");
+for(var i=0;i<tiles.length;i++)
+{
+tiles[i].addEventListener("click",function(){
+	console.log("you clicked the tile");
+	var clickedcolor = this.style.background;
+	 if(clickedcolor === "rgb(255, 165, 0)")
+	{   console.log("came through");
+		this.style.background = "black";
+	}
+
+
+
+});
+}
