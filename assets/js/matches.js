e33fa12c72e354ee848d7cca89061ce4d41ca556
Ravi Varshney <ravivarshney49@gmail.com>
2018-09-21 20:11:30 +0530

    added some js functions

--- assets/js/matches.js
+++ assets/js/matches.js
@@ -1,17 +1,62 @@
 var tiles = document.querySelectorAll(".thumbnail");
-tiles.forEach(function(i){
-	i.style.background="rgb(255,165,0)";
+tiles.forEach(function (i) {
+	i.style.background = "rgb(255,165,0)";
 });
-for (var i = 0; i < tiles.length; i++) {
-	tiles[i].addEventListener("click", function () {
-		console.log("you clicked the tile");
-		var clickedcolor = this.style.background;
-		if (clickedcolor === "rgb(255, 165, 0)") {
-			console.log("came through");
-			this.style.background = "black";
-		}
+score = 0;
+var colors = [];
+driver();
+function driver() {
+	// check(0, 3);
+	colors = generateRandColors(8);
+	for (var i = 0; i < tiles.length; i++) {
+		tiles[i].addEventListener("click", function () {
+			console.log("you clicked the tile");
+			var clickedcolor = this.style.background;
+			if (clickedcolor === "rgb(255, 165, 0)") {
+				console.log("came through");
+				this.style.background = colors[0];
+			}
+		});
+	}
+}
+function check(i, j) {
+	if (tiles[i].style.background === tiles[j].style.background) {
+		tiles[i].style.background = "rgba(255, 255, 255, 0)";
+		tiles[j].style.background = "rgba(255, 255, 255, 0)";
+		tiles[i].style.border = "1px solid rgba(255, 255, 255, 0)";
+		tiles[j].style.border = "1px solid rgba(255, 255, 255, 0)";
+		score = score+10;
+	}
+	else {
+		tiles[i].style.background = "rgb(255,165,0)";
+		tiles[j].style.background = "rgb(255,165,0)";
+		score = score - 5
+	}
+}
 
+function reset() {
 
+}
+function randomColor() {
+	//pick r, g, b from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
 
-	});
+function generateRandColors(num) {
+	//make an array
+	var arr = [];
+	//repeat num times
+	for (var i = 0; i < num; i++) {
+		//get random numbers and push it in array
+		arr.push(randomColor());
+	}
+	var uniques = chance.unique(chance.natural, 8, {min: 0, max: 7});
+	for(i=0;i<8;i++){
+		arr.push(arr[uniques[i]]);
+	}
+	//return that array
+	return arr;
 }
