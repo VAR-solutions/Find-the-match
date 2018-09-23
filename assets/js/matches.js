0b30f75a9e7d09e98d98c1eb71d77269c3803154
Vishal Gaur <rr.vishalgaur@gmail.com>
2018-09-23 14:59:40 +0530

    updated files

--- assets/js/matches.js
+++ assets/js/matches.js
@@ -1,14 +1,65 @@
+var score = 0;
+var count = 0;
+var flag = 0;
+var colors = [];
 var tiles = document.querySelectorAll(".thumbnail");
 var scoreDisplay = document.querySelector("#score");
 
 tiles.forEach(function (i) {
 	i.style.background = "black";
 });
-var score = 0;
-var count = 0;
-var flag = 0;
-var colors = [];
+
 driver();
+
+function driver() {
+	// check(0, 3);
+	colors = generateRandColors(8);
+	score = 25;
+	for (var i = 0; i < tiles.length; i++) {
+		tiles[i].addEventListener("click", function () {
+			scoreDisplay.textContent = score;
+			var clickedcolor = this.style.background;
+			if(this.style.border === ""){
+				this.style.border = "2px solid black";
+			}
+			if (clickedcolor === "black") {
+				count += 1;
+				this.style.background = col();
+				if(count==2){
+					var i = ind();
+					var j = ind();
+					setTimeout(function(){
+						if (tiles[i].style.background === tiles[j].style.background) {
+							tiles[i].style.background = "rgba(255, 255, 255, 0)";
+							tiles[j].style.background = "rgba(255, 255, 255, 0)";
+							tiles[i].style.border = "1px solid rgba(255, 255, 255, 0)";
+							tiles[j].style.border = "1px solid rgba(255, 255, 255, 0)";
+							tiles[i].blur = "";
+							tiles[i].style.zIndex = "";
+							tiles[j].blur = "";
+							tiles[j].style.zIndex = "";
+							score = score+25;
+						}
+						else {
+							tiles[i].style.background = "black";
+							tiles[j].style.background = "black";
+							tiles[i].blur = "";
+							tiles[i].style.zIndex = "";
+							tiles[j].blur = "";
+							tiles[j].style.zIndex = "";
+							tiles[i].style.border = ""
+							tiles[j].style.border = "";
+							score = score - 5
+						}
+						scoreDisplay.textContent = score;
+					},250);
+					count = 0;
+				}
+			}
+		});
+	}
+}
+
 function ind() {
 	for(i=0;i<16;i++){
 		if(tiles[i].style.border === "2px solid black" & tiles[i].blur!= "2"){
@@ -16,58 +67,6 @@ function ind() {
 			return i;
 		}
 	}
-}
-function driver() {
-	// check(0, 3);
-	colors = generateRandColors(8);
-	score = 25;
-	for (var i = 0; i < tiles.length; i++) {
-		tiles[i].addEventListener("click", function () {
-			console.log("you clicked the tile");
-			scoreDisplay.textContent = score;
-			var clickedcolor = this.style.background;
-			if(this.style.border === "")
-			{
-			this.style.border = "2px solid black"; }
-			if (clickedcolor === "black") {
-				count += 1;
-				this.style.background = col();
-                if(count==2){
-                    var i = ind();
-                    var j = ind();
-                    setTimeout(function(){
-                        if (tiles[i].style.background === tiles[j].style.background) {
-                            tiles[i].style.background = "rgba(255, 255, 255, 0)";
-                            tiles[j].style.background = "rgba(255, 255, 255, 0)";
-                            tiles[i].style.border = "1px solid rgba(255, 255, 255, 0)";
-                            tiles[j].style.border = "1px solid rgba(255, 255, 255, 0)";
-                            tiles[i].blur = "";
-                            tiles[i].style.zIndex = "";
-                            tiles[j].blur = "";
-                            tiles[j].style.zIndex = "";
-                            score = score+25;
-                        }
-                        else {
-                            tiles[i].style.background = "black";
-                            tiles[j].style.background = "black";
-                            tiles[i].blur = "";
-                            tiles[i].style.zIndex = "";
-                            tiles[j].blur = "";
-                            tiles[j].style.zIndex = "";
-                            tiles[i].style.border = ""
-                            tiles[j].style.border = "";
-                            score = score - 5
-                        }
-												scoreDisplay.textContent = score;
-					},250);
-                    count = 0;
-                }
-			}
-
-
-		});
-	}
-
 }
 
 function col(){
@@ -81,42 +80,8 @@ function col(){
 	}
 	return "black";
 }
-// function check(i, j) {
-// 	if (tiles[i].style.background === tiles[j].style.background) {
-// 		tiles[i].style.background = "rgba(255, 255, 255, 0)";
-// 		tiles[j].style.background = "rgba(255, 255, 255, 0)";
-// 		tiles[i].style.border = "1px solid rgba(255, 255, 255, 0)";
-// 		tiles[j].style.border = "1px solid rgba(255, 255, 255, 0)";
-//         tiles[i].blur = "";
-//         tiles[i].style.zIndex = "";
-//         tiles[j].blur = "";
-//         tiles[j].style.zIndex = "";
-// 		score = score+10;
-// 	}
-// 	else {
-// 		tiles[i].style.background = "black";
-// 		tiles[j].style.background = "black";
-// 		tiles[i].blur = "";
-// 		tiles[i].style.zIndex = "";
-//         tiles[j].blur = "";
-//         tiles[j].style.zIndex = "";
-//         tiles[i].style.border = ""
-//         tiles[j].style.border = "";
-// 		score = score - 5;
-// 	}
-// }
-//
-// function reset() {
-//
-// }
-function randomColor() {
-	//pick r, g, b from 0 - 255
-	var r = Math.floor(Math.random() * 256);
-	var g = Math.floor(Math.random() * 256);
-	var b = Math.floor(Math.random() * 256);
-	return "rgb(" + r + ", " + g + ", " + b + ")";
-}
 
+// make array of random colors
 function generateRandColors(num) {
 	//make an array
 	var arr = [];
@@ -133,8 +98,17 @@ function generateRandColors(num) {
 	var as = [];
 	var uniques = chance.unique(chance.natural, 16, {min: 0, max: 15});
 	for (var i = 0; i < 16; i++) {
-   	as.push(arr[uniques[i]]);
+		as.push(arr[uniques[i]]);
 
 	}
 	return as;
+}
+
+//function to generate random color
+function randomColor() {
+	//pick r, g, b from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
